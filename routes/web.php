5a11b4579d2955099061c3e8a86cfb6703eb20d0
Matheus <matheussandi@hotmail.com>
2024-05-20 12:30:44 -0300

    feat: add admin page

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,23 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ThanksController;
 use App\Http\Controllers\LeadController;
+use App\Http\Controllers\AuthController;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('home');
 
 Route::post('/agradecimentos', [LeadController::class, 'store'])->name('lead.store');
+
+Route::name('admin.')->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('index');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/leads', [LeadController::class, 'index'])->name('lead');
+    Route::get('/leads/exportar', [LeadController::class, 'export'])->name('lead.export');
+});
+
 
 Route::fallback(function () {
     return redirect()->route('home');
